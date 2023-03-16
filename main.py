e321f775d208cd0c114005b38304ca6e9f661d5f
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-03-16 09:11:20 +0000

    commit v1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from cryptography.fernet import Fernet
-#from PySimpleGUI import PySimpleGUI as sg
+# from PySimpleGUI import PySimpleGUI as sg
 
-#chave = Fernet.generate_key()
+# chave = Fernet.generate_key()
 
-#with open('chave.key', 'wb') as filekey:
+# with open('chave.key', 'wb') as filekey:
 #    filekey.write(chave)
 
 with open('Criptografar_e_Descriptografar/chave.key', 'rb') as filekey:
@@ -11,15 +11,18 @@ with open('Criptografar_e_Descriptografar/chave.key', 'rb') as filekey:
 
 fernet = Fernet(chave)
 
-#with open('Criptografar_e_Descriptografar/arquivo.txt', 'rb') as arquivo:
-#    conteudo_arquivo = arquivo.read()
+with open('Criptografar_e_Descriptografar/arquivo.txt', 'rb') as arquivo:
+    conteudo_arquivo = arquivo.read()
 
-#criptografado = fernet.encrypt(conteudo_arquivo)
+criptografado = fernet.encrypt(conteudo_arquivo)
 
-#with open('Criptografar_e_Descriptografar/arquivo1.txt', 'wb') as arquivo_criptografado:
-#    arquivo_criptografado.write(criptografado)# type: ignore
-    
-with open('Criptografar_e_Descriptografar/arquivo1.txt', 'rb') as arquivo_criptografado:
-    criptografado = arquivo_criptografado.read()
-    
-descriptografado = fernet.decrypt(criptografado)
+with open('Criptografar_e_Descriptografar/arquivo1.txt', 'wb') as arquivo_criptografado:
+    arquivo_criptografado.write(criptografado)# type: ignore
+
+#with open('Criptografar_e_Descriptografar/arquivo1.txt', 'rb') as arquivo_criptografado:
+#    criptografado = arquivo_criptografado.read()
+
+#descriptografado = fernet.decrypt(criptografado)
+
+#with open('Criptografar_e_Descriptografar/arquivo1.txt', 'wb') as arquivo_descriptografado:
+#    arquivo_descriptografado.write(descriptografado)  # type: ignore
