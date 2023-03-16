e8cbe48a597d13928edd03f8d88ed534a6bd5f61
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-03-16 07:45:34 +0000

    decrypt_test commit

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ with open('Criptografar_e_Descriptografar/chave.key', 'rb') as filekey:
 
 fernet = Fernet(chave)
 
-with open('Criptografar_e_Descriptografar/arquivo.txt', 'rb') as arquivo:
-    conteudo_arquivo = arquivo.read()
+#with open('Criptografar_e_Descriptografar/arquivo.txt', 'rb') as arquivo:
+#    conteudo_arquivo = arquivo.read()
 
-criptografado = fernet.encrypt(conteudo_arquivo)
+#criptografado = fernet.encrypt(conteudo_arquivo)
 
-with open('Criptografar_e_Descriptografar/arquivo1.txt', 'wb') as arquivo_criptografado:
-    arquivo_criptografado.write(criptografado)# type: ignore
+#with open('Criptografar_e_Descriptografar/arquivo1.txt', 'wb') as arquivo_criptografado:
+#    arquivo_criptografado.write(criptografado)# type: ignore
     
 with open('Criptografar_e_Descriptografar/arquivo1.txt', 'rb') as arquivo_criptografado:
     criptografado = arquivo_criptografado.read()
